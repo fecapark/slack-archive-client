c758f1fb524ca5e3cd49a2fdc23a18b64cdc2a35
fecapark <fecapark@gmail.com>
2025-03-26 15:36:17 +0900

    :lipstick: GNB > 페이지 링크 마크업 작성

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -1,13 +1,27 @@
+import Link from 'next/link'
+
 import AutoHeightImage from '@/components/AutoHeightImage'
 
 export const GNB = () => {
   return (
-    <div className="flex h-[64px] w-full items-center px-5">
-      <div className="flex items-center gap-3">
+    <div className="flex h-[64px] w-full items-center justify-between px-5 md:justify-start">
+      <div className="mr-[50px] flex items-center gap-3">
         <div className="w-[32px]">
           <AutoHeightImage alt="Logo" src="/logo.svg" unoptimized />
         </div>
         <span className="text-lg font-bold">멘션봇</span>
+      </div>
+
+      <div className="flex items-center gap-8">
+        <Link className="disabled:text-text-disabled text-lg font-extrabold" href="/">
+          홈
+        </Link>
+        <Link className="text-text-disabled text-lg font-extrabold" href="/">
+          키워드
+        </Link>
+        <Link className="text-text-disabled text-lg font-extrabold" href="/">
+          아카이브
+        </Link>
       </div>
     </div>
   )
