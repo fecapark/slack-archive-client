5634de67943847a9f72ecfd325529651144f0041
fecapark <fecapark@gmail.com>
2025-03-28 04:00:57 +0900

    :sparkles: 사이드바 채널 선택 시 스타일 추가

--- src/app/archives/components/SidebarChannelItem.tsx
+++ src/app/archives/components/SidebarChannelItem.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { tv } from 'tailwind-variants'
 
 import { ChannelItem } from '@/apis/channels'
 import { SidebarChannelIcon } from '@/app/archives/components/Icons/SidebarChannelIcon'
@@ -7,15 +11,32 @@ interface ChannelItemProps {
   channel: ChannelItem
 }
 
+const channelItem = tv({
+  slots: {
+    link: 'hover:bg-grey100 group text-text-secondary flex cursor-pointer items-center gap-2 rounded-sm p-2',
+    icon: 'group-hover:text-text-primary size-4',
+    text: 'group-hover:text-text-primary text-sm font-medium',
+  },
+  variants: {
+    active: {
+      true: {
+        link: 'bg-grey100 text-text-primary',
+        text: 'font-bold',
+      },
+    },
+  },
+})
+
 export const SidebarChannelItem = ({ channel }: ChannelItemProps) => {
+  const pathname = usePathname()
+  const isItemActive = pathname.includes(channel.id)
+
+  const { icon, link, text } = channelItem({ active: isItemActive })
+
   return (
-    <Link
-      className="hover:bg-grey100 group text-text-secondary flex cursor-pointer items-center gap-2 rounded-sm p-2"
-      href={`/archives/${channel.id}`}
-      key={channel.id}
-    >
-      <SidebarChannelIcon className="group-hover:text-text-primary size-4" />
-      <div className="group-hover:text-text-primary text-sm font-medium">{channel.name}</div>
+    <Link className={link()} href={`/archives/${channel.id}`} key={channel.id}>
+      <SidebarChannelIcon className={icon()} />
+      <div className={text()}>{channel.name}</div>
     </Link>
   )
 }
