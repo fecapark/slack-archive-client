558f1e826c35de1e5a6e5bbe4a39dfcb6d609070
fecapark <fecapark@gmail.com>
2025-03-28 05:13:30 +0900

    :bug: 슬랙 마크다운 > 인라인 코드가 항상 볼드인 문제 수정

--- src/components/SlackMarkdown/components/Code.tsx
+++ src/components/SlackMarkdown/components/Code.tsx
@@ -6,7 +6,7 @@ export const Code = ({ className, children, ...props }: React.HTMLAttributes<HTM
       {...props}
       className={clsx(
         className,
-        'm-[1px] h-fit rounded-[3px] border border-[rgba(29,28,29,0.13)] bg-[rgba(29,28,29,0.04)] px-[3px] pt-[2px] pb-[2px] text-xs font-bold text-[rgb(192,19,67)]'
+        'm-[1px] h-fit rounded-[3px] border border-[rgba(29,28,29,0.13)] bg-[rgba(29,28,29,0.04)] px-[3px] pt-[2px] pb-[2px] text-xs text-[rgb(192,19,67)]'
       )}
     >
       {children}
