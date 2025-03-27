ccb06d5c9aaef87cea2f0e2ead580f12aaf7cf79
fecapark <fecapark@gmail.com>
2025-03-28 04:16:20 +0900

    :sparkles: 채널 아이템 api 구현

--- src/apis/channels.ts
+++ src/apis/channels.ts
@@ -16,3 +16,8 @@ export const getChannels = async () => {
   const res = await api.get<ChannelItem[]>('api/channels').json()
   return ChannelListSchema.parse(res)
 }
+
+export const getChannel = async (channelId: string) => {
+  const res = await api.get<ChannelItem>(`api/channel/${channelId}`).json()
+  return ChannelSchema.parse(res)
+}
