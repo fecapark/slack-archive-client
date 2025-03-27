c8dae7e673f8ccad882a4a3a5e473a02b54e1190
fecapark <fecapark@gmail.com>
2025-03-28 03:49:52 +0900

    :sparkles: 채널 목록 api 구현

---
+++ src/apis/channels.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+import { api } from '@/apis/api'
+
+export const ChannelSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string().optional(),
+})
+
+export const ChannelListSchema = z.array(ChannelSchema)
+
+export type ChannelItem = z.infer<typeof ChannelSchema>
+
+export const getChannels = async () => {
+  const res = await api.get<ChannelItem[]>('api/channels').json()
+  return ChannelListSchema.parse(res)
+}
