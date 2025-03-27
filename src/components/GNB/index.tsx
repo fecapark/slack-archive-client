51e8bcc9fec989898afda0718e4e304aa7abe19d
fecapark <fecapark@gmail.com>
2025-03-27 11:54:26 +0900

    :sparkles: 로고 개선
    - 멘션폿 폰트 변경
    - 드래그 못하게 변경
    
    .

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -1,15 +1,20 @@
+import { Do_Hyeon } from 'next/font/google'
+import Link from 'next/link'
+
 import AutoHeightImage from '@/components/AutoHeightImage'
 import { LinkList } from '@/components/GNB/LinkList'
+
+const DoHyeon = Do_Hyeon({ subsets: ['latin'], weight: '400' })
 
 export const GNB = () => {
   return (
     <header className="flex h-[64px] w-full shrink-0 items-center justify-between px-5 md:justify-start">
-      <div className="mr-[50px] flex items-center gap-3">
+      <Link className="mr-[50px] flex items-center gap-3 select-none" href="/">
         <div className="w-[32px]">
           <AutoHeightImage alt="Logo" src="/logo.svg" unoptimized />
         </div>
-        <span className="text-lg font-bold">멘션봇</span>
-      </div>
+        <span className={`text-xl ${DoHyeon.className}`}>멘션봇</span>
+      </Link>
 
       <div className="flex items-center gap-8">
         <LinkList />
