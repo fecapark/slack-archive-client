bb456009ba9c5165f1466591ab5ef0119195910b
fecapark <fecapark@gmail.com>
2025-03-28 06:31:19 +0900

    .

--- src/app/archives/[channelId]/layout.tsx
+++ src/app/archives/[channelId]/layout.tsx
@@ -10,39 +10,42 @@ interface ChannelLayoutProps {
   }>
 }
 
-const ChannelLayout = async ({ params }: ChannelLayoutProps) => {
+const ChannelLayout = async ({ params, children }: React.PropsWithChildren<ChannelLayoutProps>) => {
   const { channelId } = await params
   const channel = await getChannel(channelId)
   const threads = await getThreads(channelId)
 
   return (
-    <ArchivePannel
-      className="w-full"
-      description={channel.description}
-      title={
-        <div className="flex items-center">
-          <SidebarChannelIcon className="mr-1 size-4" /> {channel.name}
-        </div>
-      }
-    >
-      <div className="flex flex-col">
-        {threads.map(({ head }) => (
-          <div
-            className="hover:bg-grey100 ease-ease cursor-pointer rounded-md px-4 py-3 transition-colors duration-300"
-            key={head.ts}
-          >
-            <SlackMessageItem
-              createdAt={head.ts}
-              createdAtFormat="2월 3일, 오후 10:23"
-              profileImageUrl={head.user.avatar}
-              username={head.user.name}
-            >
-              {head.text}
-            </SlackMessageItem>
+    <>
+      <ArchivePannel
+        className="w-full"
+        description={channel.description}
+        title={
+          <div className="flex items-center">
+            <SidebarChannelIcon className="mr-1 size-4" /> {channel.name}
           </div>
-        ))}
-      </div>
-    </ArchivePannel>
+        }
+      >
+        <div className="flex flex-col">
+          {threads.map(({ head }) => (
+            <div
+              className="hover:bg-grey100 ease-ease cursor-pointer rounded-md px-4 py-3 transition-colors duration-300"
+              key={head.ts}
+            >
+              <SlackMessageItem
+                createdAt={head.ts}
+                createdAtFormat="2월 3일, 오후 10:23"
+                profileImageUrl={head.user.avatar}
+                username={head.user.name}
+              >
+                {head.text}
+              </SlackMessageItem>
+            </div>
+          ))}
+        </div>
+      </ArchivePannel>
+      {children}
+    </>
   )
 }
 
