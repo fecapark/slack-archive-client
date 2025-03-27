6069cfd4f145d2602b91b8e387243ddaadf3c77d
fecapark <fecapark@gmail.com>
2025-03-28 05:54:48 +0900

    :sparkles: 슬랙 마크다운 폰트를 NotoSansKR로 변경

--- src/components/SlackMarkdown/index.tsx
+++ src/components/SlackMarkdown/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import ReactMarkdown, { RuleType } from 'markdown-to-jsx'
+import { Noto_Sans_KR } from 'next/font/google'
 import { AnchorHTMLAttributes, HTMLAttributes } from 'react'
 
 import { Code } from '@/components/SlackMarkdown/components/Code'
@@ -19,6 +20,10 @@ import { parseDataset } from '@/components/SlackMarkdown/utils/dataset'
 import { tramsformToHTMLAttributes } from '@/components/SlackMarkdown/utils/transform'
 import { assertNonNullish } from '@/utils/assertion'
 
+const NotoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+})
+
 export const SlackMarkdown = ({ children }: { children: string }) => {
   const converts = [
     convertMentionString,
@@ -28,41 +33,47 @@ export const SlackMarkdown = ({ children }: { children: string }) => {
   ]
 
   return (
-    <ReactMarkdown
-      className="text-[#1d1c1d]"
-      options={{
-        renderRule(next, node, _, state) {
-          if (node.type === RuleType.codeInline && node.text.startsWith(magicCodeBlockString)) {
-            const rawText = String.raw`${node.text.replace(magicCodeBlockString, '')}`
-            return <CodeBlock key={state.key}>{rawText}</CodeBlock>
-          }
-          return next()
-        },
-        overrides: {
-          code: (props) => <Code {...props} />,
-          // Next.js Image로 폴백해줘요.
-          img: (props) => <MdImage {...props} alt={props.alt} />,
-          span: (props) => {
-            const p = tramsformToHTMLAttributes<HTMLAttributes<HTMLSpanElement>>(props)
-            const dataset = parseDataset(p)
-
-            if (dataset.mention) {
-              return <MentionSpan {...p} />
-            }
-            return <Span {...p} />
-          },
-          p: ({ children }) => <>{children}</>,
-          b: ({ children }) => <em>{children}</em>,
-          em: ({ children }) => <b>{children}</b>,
-          a: (props) => {
-            const p = tramsformToHTMLAttributes<AnchorHTMLAttributes<HTMLAnchorElement>>(props)
-            assertNonNullish(p.href)
-            return <MdLink {...p} href={p.href} />
-          },
-        },
+    <div
+      className={`line-height-[22.0002px] text-[15px] text-[#1d1c1d] ${NotoSansKR.className} antialiased`}
+      style={{
+        fontVariantLigatures: 'common-ligatures',
       }}
     >
-      {children && converts.reduce((acc, convert) => convert(acc), children)}
-    </ReactMarkdown>
+      <ReactMarkdown
+        options={{
+          renderRule(next, node, _, state) {
+            if (node.type === RuleType.codeInline && node.text.startsWith(magicCodeBlockString)) {
+              const rawText = String.raw`${node.text.replace(magicCodeBlockString, '')}`
+              return <CodeBlock key={state.key}>{rawText}</CodeBlock>
+            }
+            return next()
+          },
+          overrides: {
+            code: (props) => <Code {...props} />,
+            // Next.js Image로 폴백해줘요.
+            img: (props) => <MdImage {...props} alt={props.alt} />,
+            span: (props) => {
+              const p = tramsformToHTMLAttributes<HTMLAttributes<HTMLSpanElement>>(props)
+              const dataset = parseDataset(p)
+
+              if (dataset.mention) {
+                return <MentionSpan {...p} />
+              }
+              return <Span {...p} />
+            },
+            p: ({ children }) => <>{children}</>,
+            b: ({ children }) => <em>{children}</em>,
+            em: ({ children }) => <b>{children}</b>,
+            a: (props) => {
+              const p = tramsformToHTMLAttributes<AnchorHTMLAttributes<HTMLAnchorElement>>(props)
+              assertNonNullish(p.href)
+              return <MdLink {...p} href={p.href} />
+            },
+          },
+        }}
+      >
+        {children && converts.reduce((acc, convert) => convert(acc), children)}
+      </ReactMarkdown>
+    </div>
   )
 }
