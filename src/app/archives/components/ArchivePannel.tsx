54a6ee7e52f96bb68d9cb619b4c55706ffcf61d0
fecapark <fecapark@gmail.com>
2025-03-29 02:19:15 +0900

    :sparkles: 쓰레드 메시지 목록 렌더링 구현

--- src/app/archives/components/ArchivePannel.tsx
+++ src/app/archives/components/ArchivePannel.tsx
@@ -13,7 +13,7 @@ export const ArchivePannel = ({
   className,
 }: React.PropsWithChildren<ArchivePannelProps>) => {
   return (
-    <div className={clsx('h-full rounded-2xl bg-white p-2', className)}>
+    <div className={clsx('flex h-full flex-col rounded-2xl bg-white p-2', className)}>
       <div className="flex items-center gap-1">
         <div className="mr-1 p-2 text-base font-bold">{title}</div>
         <div className="text-grey600 text-[13px]">{description}</div>
