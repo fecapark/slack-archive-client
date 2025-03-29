f113e499301ed737a73aa371b9256bd8d47f487f
fecapark <fecapark@gmail.com>
2025-03-29 10:30:35 +0900

    :sparkles: 이미지 및 비디오 요청 api 구현

---
+++ src/utils/file.ts
@@ -0,0 +1,9 @@
+export const getFileType = (mimetype: string) => {
+  if (mimetype.startsWith('image/')) {
+    return 'image'
+  }
+  if (mimetype.startsWith('video/')) {
+    return 'video'
+  }
+  return 'unknown'
+}
