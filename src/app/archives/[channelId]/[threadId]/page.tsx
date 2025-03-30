08fcd462976bbf4a61f1f39ebe0e0caa97b36a41
fecapark <fecapark@gmail.com>
2025-03-31 00:36:25 +0900

    :lipstick: 스레드 디스크립션에 아카이브된 시간 마크업

--- src/app/archives/[channelId]/[threadId]/page.tsx
+++ src/app/archives/[channelId]/[threadId]/page.tsx
@@ -2,6 +2,7 @@ import { getMessages } from '@/apis/messages'
 import { SlackThreadHeadMessageItem } from '@/app/archives/[channelId]/[threadId]/components/SlackThreadHeadMessageItem'
 import { SlackThreadMessageItem } from '@/app/archives/[channelId]/[threadId]/components/SlackThreadMessageItem'
 import { ArchivePannel } from '@/app/archives/components/ArchivePannel'
+import { formatTemplates } from '@/utils/date'
 
 interface ThreadPageProps {
   params: Promise<{
@@ -13,13 +14,20 @@ const ThreadPage = async ({ params }: ThreadPageProps) => {
   const { threadId } = await params
   const [headMessage, ...messages] = await getMessages(threadId)
 
+  const { archivedAt } = headMessage.thread
+
   const getThreadListLink = () => {
     const channelId = messages[0].channel.id
     return `/archives/${channelId}`
   }
 
   return (
-    <ArchivePannel className="w-full" closeLink={getThreadListLink()} title="쓰레드">
+    <ArchivePannel
+      className="w-full"
+      closeLink={getThreadListLink()}
+      description={`${formatTemplates['2월 3일, 오후 10:23'](archivedAt)} 에 아카이브됨`}
+      title="스레드"
+    >
       <div className="flex h-0 grow flex-col overflow-y-auto">
         <SlackThreadHeadMessageItem
           key={headMessage.ts}
