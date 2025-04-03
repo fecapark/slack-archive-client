330324f77530f82b3d9c7b98a7275073804996c3
fecapark <fecapark@gmail.com>
2025-04-03 14:59:43 +0900

    :recycle: 정적 슬랙 아이콘을 svg 렌더링으로 변경

--- src/app/(root)/components/YourssuSlackLink.tsx
+++ src/app/(root)/components/YourssuSlackLink.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import Link from 'next/link'
+
+import { IconSlack } from '@/components/Icons/IconSlack'
 
 export const YourssuSlackLink = () => {
   return (
     <Link href="https://yourssu.slack.com" rel="noopener noreferrer" target="_blank">
       <div className="bg-grey100 border-grey200 group flex items-center gap-1.5 rounded-md border px-2 py-0.5">
-        <Image alt="slack" height={16} src="/slack.svg" width={16} />
+        <IconSlack className="size-4" />
         <span className="text-text-secondary font-semibold group-hover:underline">Yourssu</span>
       </div>
     </Link>
