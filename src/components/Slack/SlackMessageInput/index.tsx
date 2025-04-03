a234961ada8a550473b616c1b84e20249b6bea23
fecapark <fecapark@gmail.com>
2025-04-04 04:27:05 +0900

    :lipstick: 슬랙 메시지 커맨드 마크업 구현

--- src/components/Slack/SlackMessageInput/index.tsx
+++ src/components/Slack/SlackMessageInput/index.tsx
@@ -5,32 +5,58 @@ import { IconEmoji } from '@/components/Slack/SlackMessageInput/Icons/IconEmoji'
 import { IconFormatting } from '@/components/Slack/SlackMessageInput/Icons/IconFormatting'
 import { IconSendFilled } from '@/components/Slack/SlackMessageInput/Icons/IconSendFilled'
 import { IconWrapper } from '@/components/Slack/SlackMessageInput/IconWrapper'
+import { SlackMessageInputCommandOverlay } from '@/components/Slack/SlackMessageInputCommandOverlay'
 
-export const SlackMessageInput = ({ children }: React.PropsWithChildren<unknown>) => {
+interface SlackMessageInputProps {
+  commands?: {
+    description: string
+    name: string
+    selected?: boolean
+  }[]
+}
+
+export const SlackMessageInput = ({
+  children,
+  commands = [],
+}: React.PropsWithChildren<SlackMessageInputProps>) => {
   return (
-    <div className="w-full rounded-lg border border-[rgba(29,28,29,0.3)]">
-      <div className="px-3 py-2">{children}</div>
-      <div className="flex h-[40px] items-center justify-between pr-1 pl-1.5">
-        <div className="flex items-center">
-          <IconWrapper containerClassName="bg-[rgba(29,28,29,0.06)] rounded-full">
-            <IconAdd />
-          </IconWrapper>
-          <IconWrapper>
-            <IconFormatting />
-          </IconWrapper>
-          <IconWrapper>
-            <IconEmoji />
-          </IconWrapper>
-          <IconWrapper>
-            <IconEllipsisHorizontal />
-          </IconWrapper>
-        </div>
-        <div className="flex h-7 items-center rounded-sm bg-[#007a5a] py-0.5">
-          <div className="flex h-full items-center border-r border-white px-2">
-            <IconSendFilled className="size-4 text-white" />
+    <div
+      className="w-full"
+      style={{
+        paddingTop: commands.length * 60,
+      }}
+    >
+      <div className="relative w-full rounded-lg border border-[rgba(29,28,29,0.3)]">
+        {!!commands.length && (
+          <SlackMessageInputCommandOverlay>
+            {commands.map((props) => (
+              <SlackMessageInputCommandOverlay.Item key={props.name} {...props} />
+            ))}
+          </SlackMessageInputCommandOverlay>
+        )}
+        <div className="px-3 py-2">{children}</div>
+        <div className="flex h-[40px] items-center justify-between pr-1 pl-1.5">
+          <div className="flex items-center">
+            <IconWrapper containerClassName="bg-[rgba(29,28,29,0.06)] rounded-full">
+              <IconAdd />
+            </IconWrapper>
+            <IconWrapper>
+              <IconFormatting />
+            </IconWrapper>
+            <IconWrapper>
+              <IconEmoji />
+            </IconWrapper>
+            <IconWrapper>
+              <IconEllipsisHorizontal />
+            </IconWrapper>
           </div>
-          <div className="flex h-full items-center px-2 py-0.5">
-            <IconCaretDown className="size-4 text-white" />
+          <div className="flex h-7 items-center rounded-sm bg-[#007a5a] py-0.5">
+            <div className="flex h-full items-center border-r border-white px-2">
+              <IconSendFilled className="size-4 text-white" />
+            </div>
+            <div className="flex h-full items-center px-2 py-0.5">
+              <IconCaretDown className="size-4 text-white" />
+            </div>
           </div>
         </div>
       </div>
