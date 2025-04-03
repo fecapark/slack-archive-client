7c8b8a4f4816f3e7a24ea83e47b0b79436431e7d
fecapark <fecapark@gmail.com>
2025-04-03 16:17:18 +0900

    :sparkles: Toast 구현

---
+++ src/components/Providers/ToastProvider/index.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { useEffect, useRef, useState } from 'react'
+
+import { ToastContext, ToastItem } from '@/components/Providers/ToastProvider/context'
+
+interface ToastProviderProps {
+  duration: number
+}
+
+const ToastAnimationGroup = dynamic(
+  () =>
+    import('@/components/Providers/ToastProvider/ToastAnimationGroup').then(
+      (m) => m.ToastAnimationGroup
+    ),
+  {
+    ssr: false,
+  }
+)
+
+export const ToastProvider = ({
+  duration,
+  children,
+}: React.PropsWithChildren<ToastProviderProps>) => {
+  const timeouts = useRef<ReturnType<typeof setTimeout>[]>([])
+  const [toasts, setToasts] = useState<ToastItem[]>([])
+
+  useEffect(() => {
+    const current = timeouts.current
+    return () => {
+      current.forEach((timeout) => clearTimeout(timeout))
+    }
+  }, [])
+
+  const addToast = (text: string) => {
+    const id = crypto.randomUUID()
+    setToasts((prev) => [...prev, { id, text }])
+
+    const timeout = setTimeout(() => {
+      setToasts((prev) => prev.filter((toast) => toast.id !== id))
+    }, duration)
+
+    timeouts.current.push(timeout)
+  }
+
+  return (
+    <ToastContext.Provider value={{ addToast, toasts }}>
+      {children}
+      <ToastAnimationGroup />
+    </ToastContext.Provider>
+  )
+}
--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,10 +1,13 @@
 import { BaseBackgroundColorProvider } from '@/components/Providers/BaseBackgroundColorProvider'
 import { TanstackQueryProvider } from '@/components/Providers/TanstackQueryProvider'
+import { ToastProvider } from '@/components/Providers/ToastProvider'
 
 export const Providers = ({ children }: React.PropsWithChildren<unknown>) => {
   return (
     <BaseBackgroundColorProvider>
-      <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      <ToastProvider duration={3000}>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </ToastProvider>
     </BaseBackgroundColorProvider>
   )
 }
