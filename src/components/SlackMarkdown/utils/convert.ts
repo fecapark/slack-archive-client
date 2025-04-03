071cf38cc53a48ef2b3087464558e4fa035c66ca
fecapark <fecapark@gmail.com>
2025-04-04 07:40:53 +0900

    :bug: 슬랙 마크다운 > 멘션에 그룹 멘션 유형도 추가

--- src/components/SlackMarkdown/utils/convert.ts
+++ src/components/SlackMarkdown/utils/convert.ts
@@ -18,13 +18,13 @@ const replaceABrAfterTag = (text: string, tagname: string) => {
 
 export const convertMentionString = (text: string) => {
   const wrapMentions = (text: string) => {
-    return text.replace(/(<@[^>]+>\s*)+/g, (match) => {
+    return text.replace(/(<(@|!)[^>]+>\s*)+/g, (match) => {
       return `<span class="inline-flex items-center gap-1 flex-wrap">${match}</span>`
     })
   }
 
-  return wrapMentions(text).replace(/<@[^>]+>/g, (match) => {
-    match = match.replace(/<@|>/g, '')
+  return wrapMentions(text).replace(/<(@|!)[^>]+>/g, (match) => {
+    match = match.replace(/<(@|!)|>/g, '')
     return `<span data-mention="true">@${match}</span>`
   })
 }
