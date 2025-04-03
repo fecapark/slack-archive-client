f464687c8b1c0fc99eb11d5b19033e7b172af9a2
fecapark <fecapark@gmail.com>
2025-04-03 13:33:44 +0900

    :fire: 스레드 목록 페이지 에러핸들링 제거

--- src/app/archives/[channelId]/layout.tsx
+++ src/app/archives/[channelId]/layout.tsx
@@ -3,7 +3,6 @@ import { getThreads } from '@/apis/threads'
 import { SlackThreadLinkItem } from '@/app/archives/[channelId]/components/SlackThreadLinkItem'
 import { ArchivePannel } from '@/app/archives/components/ArchivePannel'
 import { SidebarChannelIcon } from '@/app/archives/components/Icons/SidebarChannelIcon'
-import { handleError } from '@/utils/error'
 
 interface ChannelLayoutProps {
   params: Promise<{
@@ -18,37 +17,33 @@ const fetchChannelPageData = async (channelId: string) => {
 
 const ChannelLayout = async ({ params, children }: React.PropsWithChildren<ChannelLayoutProps>) => {
   const { channelId } = await params
+  const { channel, threads } = await fetchChannelPageData(channelId)
 
-  try {
-    const { channel, threads } = await fetchChannelPageData(channelId)
-    return (
-      <div className="flex items-center gap-2">
-        <ArchivePannel
-          className="w-full"
-          description={channel.description}
-          title={
-            <div className="flex items-center">
-              <SidebarChannelIcon className="mr-1 size-4" /> {channel.name}
-            </div>
-          }
-        >
-          <div className="flex h-0 grow flex-col overflow-y-auto">
-            {threads.map(({ head, archivedAt, metadata }) => (
-              <SlackThreadLinkItem
-                archivedAt={archivedAt}
-                head={head}
-                key={head.ts}
-                metadata={metadata}
-              />
-            ))}
+  return (
+    <div className="flex items-center gap-2">
+      <ArchivePannel
+        className="w-full"
+        description={channel.description}
+        title={
+          <div className="flex items-center">
+            <SidebarChannelIcon className="mr-1 size-4" /> {channel.name}
           </div>
-        </ArchivePannel>
-        {children}
-      </div>
-    )
-  } catch (e) {
-    return JSON.stringify(await handleError(e))
-  }
+        }
+      >
+        <div className="flex h-0 grow flex-col overflow-y-auto">
+          {threads.map(({ head, archivedAt, metadata }) => (
+            <SlackThreadLinkItem
+              archivedAt={archivedAt}
+              head={head}
+              key={head.ts}
+              metadata={metadata}
+            />
+          ))}
+        </div>
+      </ArchivePannel>
+      {children}
+    </div>
+  )
 }
 
 export default ChannelLayout
