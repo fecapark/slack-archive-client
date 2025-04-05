bb97ddb873137fb48a375094d874a100ec371904
fecapark <fecapark@gmail.com>
2025-04-05 09:20:08 +0900

    :sparkles: 링크 미리보기를 백엔드에서 불러오도록 변경

--- src/app/archives/[channelId]/components/SlackThreadLinkItem.tsx
+++ src/app/archives/[channelId]/components/SlackThreadLinkItem.tsx
@@ -1,11 +1,10 @@
-import { Suspense } from 'react'
+'use client'
 
 import { ThreadItem } from '@/apis/threads'
 import { SlackMessageMenu } from '@/app/archives/[channelId]/components/SlackMessageMenu'
 import { SlackThreadInformation } from '@/app/archives/[channelId]/components/SlackThreadInformation'
-import { useAllLinksInText } from '@/app/archives/[channelId]/hooks/useAllLinksInText'
 import { SlackMessageItem } from '@/components/Slack/SlackMessageItem'
-import { SlackMessageLinkPreview } from '@/components/Slack/SlackMessageItem/components/SlackMessageLinkPreivew'
+import { SlackMessageAttachment } from '@/components/Slack/SlackMessageItem/components/SlackMessageAttachment'
 import { SlackMessageReactionList } from '@/components/Slack/SlackMessageItem/components/SlackMessageReactionList'
 
 type SlackThreadLinkItemProps = Pick<ThreadItem, 'head' | 'metadata'> & {
@@ -13,7 +12,6 @@ type SlackThreadLinkItemProps = Pick<ThreadItem, 'head' | 'metadata'> & {
 }
 
 export const SlackThreadLinkItem = ({ head, archivedAt, metadata }: SlackThreadLinkItemProps) => {
-  const links = useAllLinksInText(head.text)
   return (
     <div className="hover:bg-grey100 ease-ease relative rounded-md px-4 py-3 transition-colors duration-300">
       <SlackMessageItem
@@ -24,12 +22,8 @@ export const SlackThreadLinkItem = ({ head, archivedAt, metadata }: SlackThreadL
       >
         <SlackMessageItem.Markdown isEdited={head.edited}>{head.text}</SlackMessageItem.Markdown>
         <SlackMessageItem.MediaList files={head.files ?? undefined} />
-        {links.map((link) => {
-          return (
-            <Suspense key={link}>
-              <SlackMessageLinkPreview url={link} />
-            </Suspense>
-          )
+        {head.attachments?.map((attachment) => {
+          return <SlackMessageAttachment attachment={attachment} key={attachment.url} />
         })}
         {head.reactions && <SlackMessageReactionList reactions={head.reactions} />}
         <SlackThreadInformation
