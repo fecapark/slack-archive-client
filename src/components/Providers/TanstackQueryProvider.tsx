873d03913da9d098a117625948a77d7c8f25f3e6
fecapark <fecapark@gmail.com>
2025-04-14 17:07:45 +0900

    :sparkles: ReactQueryDevtools 추가

--- src/components/Providers/TanstackQueryProvider.tsx
+++ src/components/Providers/TanstackQueryProvider.tsx
@@ -2,9 +2,15 @@
 
 import { getQueryClient } from '@/utils/query'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const TanstackQueryProvider = ({ children }: React.PropsWithChildren<unknown>) => {
   const queryClient = getQueryClient()
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
