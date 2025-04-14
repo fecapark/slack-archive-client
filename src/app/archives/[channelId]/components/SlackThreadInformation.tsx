74c84879e139fd22df15546acd7d518adbf636f0
fecapark <fecapark@gmail.com>
2025-04-14 23:23:25 +0900

    :lipstick: 스레드 링크 컴포넌트 반응형 깨짐 방지

--- src/app/archives/[channelId]/components/SlackThreadInformation.tsx
+++ src/app/archives/[channelId]/components/SlackThreadInformation.tsx
@@ -22,7 +22,7 @@ export const SlackThreadInformation = ({
     <Link href={`/archives/${channelId}/${threadId}`}>
       <div className="group/link ease-ease hover:border-slack-stroke-decorative mb-1 -ml-1 flex max-w-[600px] items-center justify-between rounded-md border border-transparent p-1 text-[13px] transition-colors duration-300 hover:border hover:bg-white">
         <div className="flex items-center">
-          <div className="flex items-center">
+          <div className="flex shrink-0 items-center">
             {metadata.userAvatars.map((avatar) => (
               <Image
                 alt="User Avatar"
@@ -34,10 +34,10 @@ export const SlackThreadInformation = ({
               />
             ))}
           </div>
-          <div className="text-slack-text-link ml-[3px] font-bold hover:underline">
+          <div className="text-slack-text-link ml-[3px] shrink-0 font-bold hover:underline">
             {metadata.messagesAmount - 1}개의 댓글
           </div>
-          <div className="text-slack-text-secondary ml-2">
+          <div className="text-slack-text-secondary ml-2 flex-grow overflow-hidden text-ellipsis whitespace-nowrap">
             {formatTemplates['(2024년)? 2월 3일, 오후 10:23'](archivedAt)} 에 아카이브됨
           </div>
         </div>
