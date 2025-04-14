598285abf4ef5b4376492e0afd1e91633fecb5a7
fecapark <fecapark@gmail.com>
2025-04-15 01:21:30 +0900

    :lipstick: 아카이브 페이지 > 패널과 헤더 사이 패딩 제거

--- src/app/archives/layout.tsx
+++ src/app/archives/layout.tsx
@@ -28,7 +28,7 @@ const ArchivesLayout = async ({ children }: React.PropsWithChildren<unknown>) =>
   const { channels, dehydratedState } = await fetchArchivesPageData()
 
   return (
-    <div className="relative grid h-full grid-cols-[1fr] grid-rows-[1fr] gap-2 overflow-hidden px-0 py-2 md:grid-cols-[320px_1fr] md:px-5">
+    <div className="relative grid h-full grid-cols-[1fr] grid-rows-[1fr] gap-2 overflow-hidden px-0 pb-2 md:grid-cols-[320px_1fr] md:px-5">
       <ArchivePannel className="absolute z-[1] w-full md:static" title="채널">
         <div className="flex h-0 grow flex-col gap-1 overflow-y-auto">
           {channels.map((channel) => (
