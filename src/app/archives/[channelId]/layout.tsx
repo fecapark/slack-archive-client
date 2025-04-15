b6aef72cd79e59fde86713f0a65eb48b900aab80
fecapark <fecapark@gmail.com>
2025-04-15 13:58:22 +0900

    :lipstick: 채널 스레드 목록 패널에 스켈레톤 추가

--- src/app/archives/[channelId]/layout.tsx
+++ src/app/archives/[channelId]/layout.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from 'react'
 
 import { ChannelPanel } from '@/app/archives/[channelId]/components/ChannelPanel'
+import { ArchivePannel } from '@/app/archives/components/ArchivePannel'
+import { Skeleton } from '@/components/Skeleton'
+import { SlackMessageItem } from '@/components/Slack/SlackMessageItem'
 
 interface ChannelLayoutProps {
   params: Promise<{
@@ -13,7 +16,19 @@ const ChannelLayout = async ({ params, children }: React.PropsWithChildren<Chann
 
   return (
     <div className="relative flex items-center gap-2">
-      <Suspense>
+      <Suspense
+        fallback={
+          <ArchivePannel
+            className="z-[1] w-full"
+            closeLink={`/archives/${channelId}`}
+            title={<Skeleton className="h-5 w-[240px]" />}
+          >
+            <div className="px-4 py-3">
+              <SlackMessageItem.Skeleton />
+            </div>
+          </ArchivePannel>
+        }
+      >
         <ChannelPanel channelId={channelId} />
       </Suspense>
       {children}
